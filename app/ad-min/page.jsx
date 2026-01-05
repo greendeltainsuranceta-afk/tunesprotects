83277e73e9b94f09fd69c1028b8aa3555ae4c966
greendeltainsuranceta <greendeltainsuranceta@gmail.com>
2026-01-05 06:01:50 -0700

    U

--- app/ad-min/page.jsx
+++ app/ad-min/page.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
 
-const page = () => {
-    return (
-        <div>
-            sdfsdfdf
-        </div>
-    );
-};
+"use client";
 
-export default page;
+
+export default function page() {
+  const uploadPDF = async (e) => {
+    e.preventDefault();
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const res = await fetch("/upload", {
+      method: "POST",
+      body: file,
+    });
+
+    const data = await res.json();
+    alert("Uploaded! PDF URL: " + data.url);
+      console.log(data)
+  };
+
+
+
+  return (
+    <div>
+      <h1>Upload PDF</h1>
+      <input type="file" accept="application/pdf" onChange={uploadPDF} />
+    </div>
+  );
+}
