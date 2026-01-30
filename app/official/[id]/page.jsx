b5a95e30aa5e2c1ead65da52641d0a4e92459e82
unknown <mdfaisald040@gmail.com>
2026-01-30 11:23:20 +0600

    II

--- app/official/[id]/page.jsx
+++ app/official/[id]/page.jsx
@@ -1,4 +1,4 @@
-Idersclient";
+"use client";
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import Image from "next/image";
@@ -191,17 +191,12 @@ const pdfList = [
     file: "/iders/MD-ADNAN-HABIB-ANSARI-T2P-2025-BDB2B-00013776.pdf"
   },
 
-{
-    id: "2Q758Z943WmK2d-MD-ADNAN-HABIB-ANSARI-T2P-2025-BDB2B-0001377",
-    url: "https://tunesprotect.com/official/2Q758Z943WmK2d-MD-ADNAN-HABIB-ANSARI-T2P-2025-BDB2B-0001377",
-    file: "/iders/MD-ADNAN-HABIB-ANSARI-T2P-2025-BDB2B-0001377.pdf"
-  },
 
 
 
 ];
 
-// bb
+// bb.......
   
 
   useEffect(() => {
