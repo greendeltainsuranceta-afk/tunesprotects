b226de4fd16611ab6826918bed9074988e5b30b0
greendeltainsuranceta-afk <mdrakib950dd@gmail.com>
2026-03-05 11:17:39 +0600

    Hh

--- app/page.jsx
+++ app/page.jsx
@@ -11,8 +11,8 @@ export default function Home() {
   return (
     <>
 
-
-<Header className="hidden"></Header>
+<div className="hidden">
+<Header> </Header>
 
 {/* header */}
 
@@ -110,7 +110,7 @@ Contact Us
    
 
 <Footer></Footer>
-
+</div>
     </>
   );
 }
