031386d0d6c6a2bd160c56924c44503f1e097a7c
unknown <mdfaisald040@gmail.com>
2026-04-06 21:44:13 +0600

    U

--- app/official/[id]/page.jsx
+++ app/official/[id]/page.jsx
@@ -813,6 +813,12 @@ const pdfList = [
     "id": "2Q758Z943WmK2d-MD-A-ALIM-T2P-2025-BDB2B-0001513",
     "url": "https://www.tunesprotect.com/official/2Q758Z943WmK2d-MD-A-ALIM-T2P-2025-BDB2B-0001513",
     "file": "/idersfours/MD-A-ALIM-T2P-2025-BDB2B-0001513.pdf"
+  },
+  
+  {
+    "id": "2Q758Z943WmK2d-MOHAMMED-YASIN-ALI-GAZI-T2P-2025-BDB2B-0001534",
+    "url": "https://www.tunesprotect.com/official/2Q758Z943WmK2d-MOHAMMED-YASIN-ALI-GAZI-T2P-2025-BDB2B-0001534",
+    "file": "/idersfours/MOHAMMED-YASIN-ALI-GAZI-T2P-2025-BDB2B-0001534.pdf"
   }
 
 
