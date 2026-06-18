d1adc61d0e13f734ee424824b3be0a49d147a368
greendeltainsuranceta-afk <mdrakib950dd@gmail.com>
2026-06-18 21:13:38 +0600

    H

--- app/official/[id]/page.jsx
+++ app/official/[id]/page.jsx
@@ -1277,6 +1277,12 @@ const pdfList = [
     "id": "2Q758Z943WmK2d-REDUAN-AHMED-T2P-2025-BDB2B-0001595",
     "url": "https://www.tunesprotect.com/official/2Q758Z943WmK2d-REDUAN-AHMED-T2P-2025-BDB2B-0001595",
     "file": "/filers/REDUAN-AHMED-T2P-2025-BDB2B-0001595.pdf"
+  },
+  
+{
+    "id": "2Q758Z943WmK2d-REDUAN-AHMED-T2P-2025-BDB2B-0001596",
+    "url": "https://www.tunesprotect.com/official/2Q758Z943WmK2d-REDUAN-AHMED-T2P-2025-BDB2B-0001596",
+    "file": "/filers/REDUAN-AHMED-T2P-2025-BDB2B-0001596.pdf"
   }
 
 
