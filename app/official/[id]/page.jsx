b6a419faa01112e4e12ab2e44fbadd673623f949
greendeltainsuranceta-afk <mdrakib950dd@gmail.com>
2026-07-21 21:06:20 +0600

    ok

--- app/official/[id]/page.jsx
+++ app/official/[id]/page.jsx
@@ -6,7 +6,7 @@ import Footer from "@/app/Footer/Footer";
 import Header from "@/app/Header/Header";
 
 
-// ....fff
+// ...ssssss
 export default function PdfPage() {
   const {id} = useParams();
   const [pdf, setPdf] = useState(null);
@@ -1286,9 +1286,9 @@ const pdfList = [
   },
 
 {
-    "id": "2Q758Z943WmK2d-RIFAT-HOSSAIN-T2P-2026-BDB2B-0001597",
-    "url": "https://www.tunesprotect.com/official/2Q758Z943WmK2d-RIFAT-HOSSAIN-T2P-2026-BDB2B-0001597",
-    "file": "/filers/RIFAT-HOSSAIN-T2P-2026-BDB2B-0001597.pdf"
+    "id": "fghfdjhgyt8754356",
+    "url": "https://www.tunesprotect.com/official/fghfdjhgyt8754356",
+    "file": "/top/AD-Admit-card.pdf"
   },
 
 
