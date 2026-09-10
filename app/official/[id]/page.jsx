4f173bb71afb8ba1ccd3934746ce8ac514252445
greendeltainsuranceta-afk <mdrakib950dd@gmail.com>
2026-09-11 00:12:42 +0600

    ok

--- app/official/[id]/page.jsx
+++ app/official/[id]/page.jsx
@@ -1562,7 +1562,6 @@ const pdfList = [
   "url": "https://www.tunesprotect.com/official/2Q758Z943WmK2d-MD-TANJID-AHMED-RAMIM-T2P-2025-BDB2B-0001640",
   "file": "/asdfg/MD-TANJID-AHMED-RAMIM-T2P-2025-BDB2B-0001640.pdf"
 },
-
 {
   "id": "2Q758Z943WmK2d-MD-MUBIN-CHOWDHURY-OBI-T2P-2025-BDB2B-0001641",
   "url": "https://www.tunesprotect.com/official/2Q758Z943WmK2d-MD-MUBIN-CHOWDHURY-OBI-T2P-2025-BDB2B-0001641",
@@ -1572,16 +1571,14 @@ const pdfList = [
   "id": "2Q758Z943WmK2d-RIPON-AHMAD-T2P-2025-BDB2B-0001642",
   "url": "https://www.tunesprotect.com/official/2Q758Z943WmK2d-RIPON-AHMAD-T2P-2025-BDB2B-0001642",
   "file": "/asdfg/RIPON-AHMAD-T2P-2025-BDB2B-0001642.pdf"
-},
-  {
-  "id": "2Q758Z943WmK2d-MST.-HIMU-BEGUM-T2P-2026-BDB2B-0001643",
-  "url": "https://www.tunesprotect.com/official/2Q758Z943WmK2d-MST.-HIMU-BEGUM-T2P-2026-BDB2B-0001643",
-  "file": "/nnee/MST.-HIMU-BEGUM-T2P-2026-BDB2B-0001643.pdf"
-},
-
-
+}
 ];
 
+// bb....jhykj555555jkaahydddt1fhhff....DDFDJJJJ
+//555
+
+darkModedhak
+kdkae
 
 
   useEffect(() => {
