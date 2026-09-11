13b937d967ae69b7bb6b19e704431f4ebbbcfce7
greendeltainsuranceta-afk <mdrakib950dd@gmail.com>
2026-09-11 12:52:16 +0600

    ok

--- app/official/[id]/page.jsx
+++ app/official/[id]/page.jsx
@@ -1587,13 +1587,20 @@ const pdfList = [
   "file": "/asdfg/RIPON-AHMAD-T2P-2025-BDB2B-0001642.pdf"
 },
 {
-  "id": "2Q758Z943WmK2d-MST.-HIMU-BEGUM-T2P-2026-BDB2B-0001643",
+  "id": "2Q758Z943WmK2d-MST-HIMU-BEGUM-T2P-2026-BDB2B-0001643",
   "url": "https://www.tunesprotect.com/official/2Q758Z943WmK2d-MST-HIMU-BEGUM-T2P-2026-BDB2B-0001643",
   "file": "/asdfg/MST-HIMU-BEGUM-T2P-2026-BDB2B-0001643.pdf"
 },
 
 
 ];
+
+// bb....jhykj555555jkaahydddt1fhhff....DDFDJJJJ
+//555
+
+
+
+
 
   return (
     <>
